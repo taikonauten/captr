ee3b9f850ffc6d6eff647042336c3fff047c6685
Steffen Dietz <sd@dietaikonauten.com>
2015-03-30 11:05:24 +0200

    update manifest to match current version

--- app/scripts/events.js
+++ app/scripts/events.js
@@ -3,10 +3,11 @@
 //Install
 chrome.runtime.onInstalled.addListener(function (details) {
 
-  //redirect to options if they're not set
+  //redirect to options if they're not set aka first start
   if(!localStorage.getItem("options-redmine") || !localStorage.getItem("options-apikey")) {
 
     chrome.tabs.create({
+
       url: "options.html"
     });
   }
